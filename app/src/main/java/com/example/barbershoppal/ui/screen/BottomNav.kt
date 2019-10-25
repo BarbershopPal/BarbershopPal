162c9cd81ab1ec8c1528be20359525a824de66b8
Blaze-Yann Manion-Mabondzot <bmanionm@gmail.com>
2019-10-26 00:58:07 +0100

    AND-2_FirstCommit: Created bottomNav and more done on BookingHostFragment

---
+++ app/src/main/java/com/example/barbershoppal/ui/screen/BottomNav.kt
@@ -0,0 +1,12 @@
+package com.example.barbershoppal.ui.screen
+
+import android.support.design.widget.BottomNavigationView
+import android.view.View
+import androidx.navigation.NavController
+
+class BottomNav(
+    private val navController: NavController,
+    private val view: View,
+    private val bottomNav: BottomNavigationView
+) {
+}
